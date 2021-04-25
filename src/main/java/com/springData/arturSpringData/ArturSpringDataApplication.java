2e6a024fdbfd183434d1676218601ce868fdff30
aszajko <aszajko25@gmail.com>
2021-04-25 14:02:03 +0200

    add my project

---
+++ src/main/java/com/springData/arturSpringData/ArturSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.springData.arturSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArturSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArturSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springData/arturSpringData/ArturSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springData.arturSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArturSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
